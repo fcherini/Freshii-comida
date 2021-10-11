083f16d84db4cf01efc2a4ab2278e305cbd9dd33
Fernanda Cherini <fer.azvdo@gmail.com>
2021-10-11 11:36:18 -0300

    Finish sections

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 $(".nav__toggler").click(() => {
   $(".nav__links").toggleClass("nav__links--show");
 });
+
+//ScrollTo
+$('a[href^="#"]').on("click", function (e) {
+  $("html, body").animate({
+    scrollTop: $(e.currentTarget.hash).scrollTop() + 100,
+  });
+  $(".nav__links").toggleClass("nav__links--show");
+});
+
+//Click card__planos
+$("cards__planos").on("click", function (e) {});
